fae9b2899a2fc992432e487fef45dbc80f000366
lhowellffh <140924934+lhowellffh@users.noreply.github.com>
2024-03-12 16:51:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { NativeModules } from "react-native";
+import React, { useEffect } from "react";
+import { NativeModules, View, Text, Linking, TouchableOpacity } from "react-native";
 const { RNCustomCode } = NativeModules;
 
 import { initialize } from "@blocklienterprise/blockli";
 import config from "@src/build_config.json";
 
 export const applyCustomCode = async (externalCodeSetup) => {
-  const blockli_config = {
-    license: "8PSLH4MCQO7P4TP", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
-    app_id: config.app_id,
-    code: externalCodeSetup,
-    //bcdn_token: "BUNNYCDN-TOKEN-AUTH-KEY",
-    website: "https://360scdhub.org", // Enter your app domain here with NO trailing slash. 
-    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
-  };
+    const blockli_config = {
+      license: "8PSLH4MCQO7P4TP",
+      app_id: config.app_id,
+      code: externalCodeSetup,
+      website: "https://360scdhub.org",
+    };
+    
+    await initialize(blockli_config);
 
-  await initialize(blockli_config);
+    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", ({ navigation }) => {
+        useEffect(() => {
+            openLink();
+            navigation.goBack();
+        }, []);
+
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Back</Text>
+                </TouchableOpacity>
+                <Text>
+                    To sign up for the the 360 SCD Hub app, please click here.
+                </Text>
+            </View>
+        );
+    });
+
+    const openLink = () => {
+        const url = "https://360scdhub.org/registration-form/";
+        Linking.openURL(url)
+            .then((supported) => {
+                if (!supported) {
+                    console.error("Opening link in an external browser is not supported");
+                }
+            })
+            .catch((err) => console.error("Error occurred while opening link: ", err));
+    };
 };
