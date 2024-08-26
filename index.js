3524dbea51d735bdc15e49c7756922faba1e30c5
Blockli Support <108662345+blockliappkit@users.noreply.github.com>
2024-08-25 22:18:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import React from "react";
-import { NativeModules } from "react-native";
-const { RNCustomCode } = NativeModules;
-
-import { initialize } from "@blocklienterprise/blockli";
-import config from "@src/build_config.json";
-
-export const applyCustomCode = async (externalCodeSetup) => {
-  const blockli_config = {
-    license: "8PSLH4MCQO7P4TP", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
-    app_id: config.app_id,
-    code: externalCodeSetup,
-    //bcdn_token: "BUNNYCDN-TOKEN-AUTH-KEY",
-    website: "https://360scdhub.org", // Enter your app domain here with NO trailing slash. 
-    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain. 
-  };
-
-  await initialize(blockli_config);
+export const applyCustomCode = (externalCodeSetup: any) => {
+	// call custom code api here
 };
